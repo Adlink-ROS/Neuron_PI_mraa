d082884e494fc4a7d0edfbc17dc5cc8dc11474fc
ChenYing Kuo <evshary@gmail.com>
2021-02-08 14:08:28 +0800

    Update mraa_init.bash

--- mraa_init.bash
+++ mraa_init.bash
@@ -6,6 +6,8 @@ else
     echo "Loading the modules..."
     sudo modprobe i2c_i801
     sudo modprobe gpio-pca953x
+    # i2c-13: SMBus I801 adapter at f040
     sudo sh -c 'echo "pca9535 0x20" > /sys/bus/i2c/devices/i2c-13/new_device'
+    # i2c-6: Synopsys DesignWare I2C adapter
     sudo sh -c 'echo "sx1509q 0x3e" > /sys/bus/i2c/devices/i2c-6/new_device'
 fi
